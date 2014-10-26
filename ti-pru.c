c1744215b2c4d7d6a954007016fa0640f7bba019
Rui Paulo <rpaulo@felyko.com>
2014-10-26 12:57:48 -0700

    ti_disassemble(): major overhaul of the disassembler part.
    Try to parse the instruction first and then the operands.  A large
    part of the instructions are similar, so this helps with the operand
    parsing.

--- ti-pru.c
+++ ti-pru.c
@@ -28,6 +28,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <inttypes.h>
+#include <stdbool.h>
 
 #include <sys/errno.h>
 #include <sys/types.h>
@@ -128,7 +130,7 @@ ti_upload(pru_t pru, unsigned int pru_number, const char *buffer, size_t size)
 		DPRINTF("IRAM at %p\n", iram);
 		memset(iram, 0, AM33XX_IRAM_SIZE);
 	}
-	DPRINTF("copying buf %p size %u\n", buffer, size);
+	DPRINTF("copying buf %p size %zu\n", buffer, size);
 	memcpy(iram, buffer, size);
 
 	return 0;
@@ -248,101 +250,288 @@ ti_initialise(pru_t pru)
 static void
 ti_reg_str(uint8_t reg, char *buf, size_t len)
 {
-	if (reg < 0xe0)
-		snprintf(buf, len, "??");
-	else
+	if (reg < 0xe0) {
+		if (reg < 32)
+			snprintf(buf, len, "r%d.b0", reg);
+		else
+			snprintf(buf, len, "r%d.w%d", reg & 0xf,
+			    reg >> 4);
+	} else
 		snprintf(buf, len, "r%d", reg - 0xe0);
+}
+
+static void
+ti_std_ins(const char *ins, const char *op1, const char *op2, const char *op3,
+    char *buf, size_t len)
+{
+	bool comma = false;
+
+	snprintf(buf, len, "%s", ins);
+	if (op1 && op1[0] != 0) {
+		snprintf(buf, len, "%s %s", buf, op1);
+		comma = true;
+	}
+	if (op2 && op2[0] != 0) {
+		snprintf(buf, len, "%s%s %s", buf, comma == true ? "," : "",
+		    op2);
+		comma = true;
+	}
+	if (op3 && op3[0] != 0)
+		snprintf(buf, len, "%s%s %s", buf, comma == true ? "," : "",
+		    op3);
 }
 
 int
 ti_disassemble(uint32_t opcode, char *buf, size_t len)
 {
-	uint8_t op;
-	size_t remlen;
+	uint8_t ins, op1, op2, op3;
+	uint16_t imm;
+	char c_op1[16], c_op2[16], c_op3[16];
+	const char *c_ins;
 
-	remlen = len;
-	op = (opcode & 0xff000000) >> 24;
-	switch (op) {
+	c_ins = NULL;
+	bzero(c_op1, sizeof(c_op1));
+	bzero(c_op2, sizeof(c_op2));
+	bzero(c_op3, sizeof(c_op3));
+	ins = (opcode & 0xff000000) >> 24;
+	op1 = (opcode & 0x00ff0000) >> 16;
+	op2 = (opcode & 0x0000ff00) >> 8;
+	op3 = opcode & 0xff;
+	/*
+	 * Parse the name of the instruction.
+	 */
+	switch (ins) {
 	case TI_OP_ADD:
-		snprintf(buf, remlen, "add");
+	case TI_OP_ADDI:
+		c_ins = "add";
 		break;
 	case TI_OP_ADC:
+	case TI_OP_ADCI:
+		c_ins = "adc";
 		break;
 	case TI_OP_SUB:
+	case TI_OP_SUBI:
+		c_ins = "sub";
 		break;
 	case TI_OP_SUC:
+	case TI_OP_SUCI:
+		c_ins = "suc";
 		break;
 	case TI_OP_LSL:
+	case TI_OP_LSLI:
+		c_ins = "lsl";
 		break;
 	case TI_OP_LSR:
+	case TI_OP_LSRI:
+		c_ins = "lsr";
 		break;
 	case TI_OP_RSB:
+	case TI_OP_RSBI:
+		c_ins = "rsb";
 		break;
 	case TI_OP_RSC:
+	case TI_OP_RSCI:
+		c_ins = "rsc";
 		break;
 	case TI_OP_AND:
+		/* An instruction following 'slp'. We ignore it. */
+		if (opcode == 0x10000000) {
+			buf[0] = 0;
+			return EINVAL;
+		}
+		if (op1 == op2)
+			c_ins = "mov";
+		else
+			c_ins = "and";
 		break;
 	case TI_OP_OR:
+	case TI_OP_ORI:
+		c_ins = "or";
 		break;
 	case TI_OP_XOR:
+	case TI_OP_XORI:
+		c_ins = "xor";
 		break;
 	case TI_OP_NOT:
+		c_ins = "not";
 		break;
 	case TI_OP_MIN:
+		c_ins = "min";
 		break;
 	case TI_OP_MAX:
+		c_ins = "max";
 		break;
 	case TI_OP_CLR:
+		c_ins = "clr";
 		break;
 	case TI_OP_SET:
+		c_ins = "set";
 		break;
 	case TI_OP_JMP:
+		if (op1 == 0x9e)
+			c_ins = "ret";
+		else
+			c_ins = "jmp";
 		break;
 	case TI_OP_JAL:
+		if (op3 == 0x9e)
+			c_ins = "call";
+		else
+			c_ins = "jal";
 		break;
 	case TI_OP_LDI:
+		c_ins = "ldi";
 		break;
 	case TI_OP_LMBD:
+		c_ins = "lmbd";
 		break;
 	case TI_OP_HALT:
+		c_ins = "halt";
 		break;
 	case TI_OP_MVI:
+		if (op1 == 0x20)
+			c_ins = "mvib";
+		else if (op1 == 0x21)
+			c_ins = "mviw";
+		else if (op1 == 0x22)
+			c_ins = "mvid";
+		else
+			c_ins = "mvi";
 		break;
 	case TI_OP_XIN:
+		c_ins = "xin";
 		break;
 	case TI_OP_XOUT:
+		c_ins = "xout";
 		break;
 	case TI_OP_SLP:
+		c_ins = "slp";
 		break;
 	case TI_OP_QBLT:
+		c_ins = "qblt";
 		break;
 	case TI_OP_QBEQ:
+		c_ins = "qbeq";
 		break;
 	case TI_OP_QBLE:
+		c_ins = "qble";
 		break;
 	case TI_OP_QBGT:
+		c_ins = "qbgt";
 		break;
 	case TI_OP_QBNE:
+		c_ins = "qbne";
 		break;
 	case TI_OP_QBGE:
+		c_ins = "qbge";
 		break;
 	case TI_OP_QBA:
+		c_ins = "qba";
 		break;
 	case TI_OP_SBCO:
+		c_ins = "sbco";
 		break;
 	case TI_OP_LBCO:
+		c_ins =  "lbco";
 		break;
 	case TI_OP_QBBC:
+		c_ins = "qbbc";
 		break;
 	case TI_OP_QBBS:
+		c_ins = "qbbs";
 		break;
 	case TI_OP_SBBO:
+		c_ins = "sbbo";
 		break;
 	case TI_OP_LBBO:
+		c_ins = "lbbo";
 		break;
 	default:
+		c_ins = "UD#";
 		break;
 	}
+	/*
+	 * Process operators.
+	 */
+	switch (ins) {
+	/* Standard 3 register instructions. */
+	case TI_OP_ADD:
+	case TI_OP_ADC:
+	case TI_OP_SUB:
+	case TI_OP_SUC:
+	case TI_OP_LSL:
+	case TI_OP_LSR:
+	case TI_OP_RSB:
+	case TI_OP_RSC:
+	case TI_OP_AND:
+	case TI_OP_OR:
+	case TI_OP_XOR:
+	case TI_OP_MIN:
+	case TI_OP_MAX:
+	case TI_OP_SET:
+	case TI_OP_LMBD:
+		ti_reg_str(op1, c_op1, sizeof(c_op1));
+		ti_reg_str(op2, c_op2, sizeof(c_op2));
+		ti_reg_str(op3, c_op3, sizeof(c_op3));
+		if (ins == TI_OP_AND && op1 == op2)
+			bzero(c_op2, sizeof(c_op2));
+		break;
+	/* Instructions with two registers. */
+	case TI_OP_NOT:
+		/* N.B. op1 is never used. */
+		ti_reg_str(op2, c_op1, sizeof(c_op1));
+		ti_reg_str(op3, c_op2, sizeof(c_op2));
+		break;
+	case TI_OP_CLR:
+		ti_reg_str(op1, c_op1, sizeof(c_op1));
+		ti_reg_str(op2, c_op2, sizeof(c_op2));
+		break;
+	/* Instructions with immediate values */
+	case TI_OP_ADDI:
+	case TI_OP_ADCI:
+	case TI_OP_SUBI:
+	case TI_OP_SUCI:
+	case TI_OP_LSLI:
+	case TI_OP_LSRI:
+	case TI_OP_RSCI:
+	case TI_OP_RSBI:
+	case TI_OP_ORI:
+	case TI_OP_XORI:
+		snprintf(c_op1, sizeof(c_op1), "0x%x", op1);
+		ti_reg_str(op2, c_op2, sizeof(c_op2));
+		ti_reg_str(op3, c_op3, sizeof(c_op3));
+		break;
+	/* Special instructions. */
+	case TI_OP_LDI:
+		imm = (opcode & 0x00ffff00) >> 8;
+		snprintf(c_op1, sizeof(c_op1), "0x%x", imm);
+		ti_reg_str(op3, c_op3, sizeof(c_op3));
+		break;
+	case TI_OP_MVI:
+		ti_reg_str(op3, c_op3, sizeof(c_op3));
+		ti_reg_str(op2, c_op2, sizeof(c_op2));
+		memmove(c_op2 + 1, c_op2, sizeof(c_op2));
+		c_op2[0] = '*';
+		break;
+	case TI_OP_JMP:
+		if (op1 != 0x9e)
+			snprintf(c_op1, sizeof(c_op1), "r%d.w0", op1 - 0x80);
+		break;
+	case TI_OP_JAL:
+		if (op3 == 0x9e) {
+			snprintf(c_op1, sizeof(c_op1), "r%d.w0", op1 - 0x80);
+		} else {
+			snprintf(c_op1, sizeof(c_op1), "r%d.w0", op1 - 0xc0);
+			snprintf(c_op3, sizeof(c_op3), "r%d.w0", op3 - 0x80);
+		}
+	case TI_OP_SLP:
+		if (op1 == 0x80)
+			snprintf(c_op1, sizeof(c_op1), "1");
+		else
+			snprintf(c_op1, sizeof(c_op1), "0");
+	}
+	/* N.B. the order of the arguments is reversed. */
+	ti_std_ins(c_ins, c_op3, c_op2, c_op1, buf, len);
 
 	return 0;
 }
